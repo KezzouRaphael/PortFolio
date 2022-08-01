7c9443fc0abd605c6b6eb45c9129c5e7f831c5d9
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-08-01 14:02:45 +0200

    Progress on home page animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LogoTitle from '../../assets/images/logo-becode.png';
 import {Link} from 'react-router-dom';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
 const Home = () => {
+
+  const[letterClass,setLetterClass] = useState('text-animate');
+  const nameArray=['R','a','p','h','a','e','l'];
+  const surnameArray=['K','e','z','z','o','u'];
+  const jobArray1 = ['W','e','b'];
+  const jobArray2 = ['D','e','v','e','l','o','p','e','r'];
+  const jobArray3 = ['a','t'];
+  useEffect(() => {
+
+        
+
+    let timeoutId = setTimeout(() => {
+
+        setLetterClass('text-animate-hover')
+
+    }, 4000)
+
+    
+
+    return () => {
+
+                clearTimeout(timeoutId)
+
+            }
+
+}, [])
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> The name is Raphael Kezzou<br/>
-        I'm a Web Developer at
+        <h1>
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i,</span>
+        <br /> 
+        <span className={`${letterClass} _13`}>I</span>
+        <span className={`${letterClass} _14`}>'m</span>
+        <br />
+        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+        <br/>
+        <AnimatedLetters letterClass={letterClass} strArray={surnameArray} idx={22}/>
+        <br/>
+        <AnimatedLetters letterClass={letterClass} strArray={jobArray1} idx={28}/>
+        <br/>
+        <AnimatedLetters letterClass={letterClass} strArray={jobArray2} idx={31}/>
+        <br/>
+        <AnimatedLetters letterClass={letterClass} strArray={jobArray3} idx={40}/>
         <img src={LogoTitle} alt="developer" />
         </h1>
         <h2>Junior Fullstack Developer</h2>
