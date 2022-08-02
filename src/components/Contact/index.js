6ee944279edbc24527ac21251258943e5b3fdd47
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-08-02 11:53:13 +0200

    Added leaftlet with my location

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 const Contact = () => {
   const[letterClass,setLetterClass] = useState('text-animate');
   const refForm = useRef();
@@ -65,6 +66,23 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Kezzou Raphael,
+          <br />
+          Belgium,
+          <br />
+          4400 Flémalle
+          <br />
+          <span><a href="mailto:Raphael.Kezzou@live.fr">Raphael.Kezzou@live.fr</a></span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[50.5994478,5.4699616]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <Marker position={[50.5994478,5.4699616]}>
+              <Popup>Je suis situé à Flémalle.</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
