24c9f59717579f000e26eff223a6e8a127c2f201
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-08-11 14:03:31 +0200

    Added cards with some projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,18 +3,47 @@ import "./Projects.scss";
 import React, { useEffect, useState } from 'react';
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import projectInfos from "../../data/portfolio.json";
 
 const Projects = () => {
   const[letterClass,setLetterClass] = useState('text-animate');
   const titleArray=['M','y',' ','P','r','o','j','e','c','t','s'];
   useEffect(() => {
     let timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 4000)
+      setLetterClass('text-animate-hover')
+    }, 3000)
     return () => {
-    clearTimeout(timeoutId);
+      clearTimeout(timeoutId)
     }
-}, [])
+  }, []);
+
+  const renderPortfolio = (portfolio) => {
+    console.log(portfolio)
+    return(
+      <div className="images-container">
+        {
+          portfolio.map((port,index)=>{
+            return(
+              <div className="project-box">              
+                <div className="title-box">
+                  <h2>{port.title}</h2>
+                </div>
+                <div className="image-box" key={index}>
+                  <img className="portfolio-image" src={port.cover} alt="project"/>
+                </div>
+                <div className="descriptions-box">
+                  <p className="description">{port.description}</p>
+                </div>
+                <div className="url-box">
+                  <a className ="link" href={port.url} target="_blank" rel="noreferrer">Link to project</a>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
   return (
     <>  
     <div className="container project-page">
@@ -22,20 +51,11 @@ const Projects = () => {
         <h1>
           <AnimatedLetters letterClass={letterClass} strArray={titleArray} idx={15}/>
         </h1>   
-        <p>Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, con Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
-        <ul className='project-lists'>
-          <li className='project-item'>
-            Projet 1
-          </li>
-          <li className='project-item'>
-            Projet 2
-          </li>
-          <li className='project-item'>
-            Projet 3
-          </li>
-        </ul>
+        <p>Voici quelques preview de mes projets vous pouvez retrouver les autres sur mon Github: </p>
+        <div>{renderPortfolio(projectInfos.portfolio)}</div>
       </div>
-    </div>
+    </div>        
+
     <Loader type="pacman"/>
     </>
   );
